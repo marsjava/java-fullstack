eff384eaacdda022ed72d3f6d1967cddeb7d8a9c
Marikannan <marikannan.cvp@gmail.com>
2021-07-17 22:51:36 +0530

    Add welcome component

--- todo-list-ui/src/app/app.component.ts
+++ todo-list-ui/src/app/app.component.ts
@@ -2,9 +2,15 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
+  // template: '<h1>{{title}}</h1>',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
+  // styles: [`
+  // h1 {
+  //   color: blueviolet;
+  // }
+  // `]
 })
 export class AppComponent {
-  title = 'todo-list-ui';
+  title = 'Todo List Component.';
 }
